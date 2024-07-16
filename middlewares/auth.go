3583b057fdcab8ef6b6999862611bb4b38b430b8
dkhvan <dkhvan.developer@gmail.com>
2024-07-16 16:07:40 +0500

    feat: Add white list ips

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -9,8 +9,21 @@ import (
 	"strings"
 )
 
+var WHITE_LIST = []string{
+	"localhost:8082",
+}
+
 func AuthMiddleware(next http.Handler, roles ...string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		clientIP := r.RemoteAddr
+
+		for _, ip := range WHITE_LIST {
+			if strings.Contains(clientIP, ip) {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+
 		token, err := verifyToken(r)
 		if err != nil || !token.Valid {
 			http.Error(w, "", http.StatusUnauthorized)
